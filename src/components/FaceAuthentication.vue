5c9f559cfbc47fbb0d9ef846f5a77694136c57e7
235 <krahs@gmai.com>
2023-10-14 14:07:30 +0300

    changeVideoOnPhone

--- src/components/FaceAuthentication.vue
+++ src/components/FaceAuthentication.vue
@@ -3,9 +3,12 @@
     <p>Upload a reference image:</p>
     <input type="file" @change="uploadReferenceImage" />
 
-    <p>Upload an image to authenticate:</p>
-    <input type="file" @change="uploadUserImage" />
-    <button @click="authenticateFace" :disabled="!userImage || !referenceImageDescriptor">Authenticate with Face</button>
+    <div v-if="referenceImageDescriptor">
+      <p>Capture an image from the camera:</p>
+      <video v-if="!userImage" ref="video" width="720" height="560" autoplay></video>
+      <img v-if="userImage" :src="capturedImageSrc" alt="Captured Image" width="720" height="560" />
+      <button @click="authenticateFace" :disabled="!userImage">Authenticate with Face</button>
+    </div>
   </div>
 </template>
 
@@ -18,7 +21,8 @@ export default {
     return {
       loadedModels: false,
       userImage: null,
-      referenceImageDescriptor: null
+      referenceImageDescriptor: null,
+      capturedImageSrc: null
     };
   },
   async mounted() {
@@ -26,11 +30,43 @@ export default {
     this.loadedModels = true;
   },
   methods: {
+    async startCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+        this.$refs.video.srcObject = stream;
+
+        // Запуск таймера для автоматической съемки через 2 секунды
+        setTimeout(this.captureImageFromCamera, 2000);
+      } catch (error) {
+        console.error("Error accessing the camera:", error);
+      }
+    },
+
+    beforeDestroy() {
+      const video = this.$refs.video;
+      if (video && video.srcObject) {
+        video.srcObject.getTracks().forEach(track => track.stop());
+      }
+    },
+
+    async captureImageFromCamera() {
+      const canvas = document.createElement('canvas');
+      canvas.width = this.$refs.video.videoWidth;
+      canvas.height = this.$refs.video.videoHeight;
+      canvas.getContext('2d').drawImage(this.$refs.video, 0, 0);
+
+      canvas.toBlob(async (blob) => {
+        this.userImage = await faceapi.bufferToImage(blob);
+        this.capturedImageSrc = URL.createObjectURL(blob);
+      }, 'image/png');
+    },
+
     async loadModels() {
       await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
       await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
       await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
     },
+
     async uploadReferenceImage(event) {
       if (this.loadedModels && event.target.files.length > 0) {
         const image = await faceapi.bufferToImage(event.target.files[0]);
@@ -42,11 +78,13 @@ export default {
         }
       }
     },
+
     async uploadUserImage(event) {
       if (this.loadedModels && event.target.files.length > 0) {
         this.userImage = await faceapi.bufferToImage(event.target.files[0]);
       }
     },
+
     async authenticateFace() {
       if (!this.userImage || !this.referenceImageDescriptor) {
         console.error("Please upload both images.");
@@ -70,7 +108,23 @@ export default {
         alert("Face not recognized.");
       }
     }
-
+  },
+  watch: {
+    referenceImageDescriptor() {
+      if (this.referenceImageDescriptor) {
+        this.startCamera();
+      }
+    }
   }
 };
 </script>
+
+<style>
+@media (max-width: 767px) {
+  video, img {
+    display: block;
+    margin: 0 auto;
+     width: 90%;
+  }
+}
+</style>
